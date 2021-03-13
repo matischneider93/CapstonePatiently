db5c4a1240fb4d3befbaf9b4577d3733007d7354
matischneider082@gmail.com <matischneider082@gmail.com>
2021-03-13 18:54:51 -0500

    Added Login Activity

--- app/src/main/java/com/mschneider/patiently/ui/activities/MainActivity.java
+++ app/src/main/java/com/mschneider/patiently/ui/activities/MainActivity.java
@@ -2,15 +2,22 @@ package com.mschneider.patiently.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.mschneider.patiently.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean loggedIn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (loggedIn == false){
+            // Starts login activity
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 }
