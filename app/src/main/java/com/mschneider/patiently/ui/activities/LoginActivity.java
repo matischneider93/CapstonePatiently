6c1fd166e457a5303e108dabf436140b09482521
matischneider082@gmail.com <matischneider082@gmail.com>
2021-03-15 08:44:44 -0400

    Added Links to different screens

--- app/src/main/java/com/mschneider/patiently/ui/activities/LoginActivity.java
+++ app/src/main/java/com/mschneider/patiently/ui/activities/LoginActivity.java
@@ -2,15 +2,42 @@ package com.mschneider.patiently.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mschneider.patiently.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private boolean loggedIn;
+    EditText userNameText;
+    EditText passwordText;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userNameText = findViewById(R.id.userNameTextField);
+        passwordText = findViewById(R.id.passwordTextField);
+        loginButton = findViewById(R.id.loginButton);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (userNameText.getText().toString().isEmpty()){
+                    loggedIn = false;
+                } else {
+                    loggedIn = true;
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    intent.putExtra("login_status", loggedIn);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/mschneider/patiently/ui/activities/MainActivity.java
+++ app/src/main/java/com/mschneider/patiently/ui/activities/MainActivity.java
@@ -4,20 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mschneider.patiently.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button physiciansButton;
+    private Button patientsButton;
+    private Button appointmentsButton;
+
     private boolean loggedIn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (loggedIn == false){
-            // Starts login activity
-            Intent intent = new Intent(this, LoginActivity.class);
+        setContentView(R.layout.activity_main);
+        physiciansButton = findViewById(R.id.physiciansButton);
+        patientsButton = findViewById(R.id.patientsButton);
+        appointmentsButton = findViewById(R.id.appointmentsButton);
+
+        physiciansButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPhysiciansActivity();
+            }
+        });
+
+        patientsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPatientsActivity();
+            }
+        });
+
+        appointmentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAppointmentsActivity();
+            }
+        });
+
+    }
+
+
+    // Button Functions for Screens
+    public void openPhysiciansActivity(){
+            Intent intent = new Intent(this, PhysiciansActivity.class);
             startActivity(intent);
-        } else {
-            setContentView(R.layout.activity_main);
         }
+    public void openPatientsActivity(){
+        Intent intent = new Intent(this, PatientsActivity.class);
+        startActivity(intent);
+    }
+    public void openAppointmentsActivity(){
+        Intent intent = new Intent(this, AppointmentsActivity.class);
+        startActivity(intent);
     }
 }
