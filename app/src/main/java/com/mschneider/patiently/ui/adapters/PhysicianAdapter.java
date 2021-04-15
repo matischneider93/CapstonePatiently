1c305a1b0664d8c3afafb6d9a5761f6a6c069106
matischneider082@gmail.com <matischneider082@gmail.com>
2021-04-15 12:58:13 -0400

    PhysicianAdapter

--- app/src/main/java/com/mschneider/patiently/ui/adapters/PhysicianAdapter.java
+++ app/src/main/java/com/mschneider/patiently/ui/adapters/PhysicianAdapter.java
@@ -1,5 +1,6 @@
 package com.mschneider.patiently.ui.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,50 +14,65 @@ import com.mschneider.patiently.models.Physician;
 
 import java.util.List;
 
-public class PhysicianAdapter  extends RecyclerView.Adapter<PhysicianAdapter.ViewHolder> {
-    List<Physician> physiciansList;
 
-    public PhysicianAdapter(List<Physician> physiciansList) {
-        this.physiciansList = physiciansList;
-    }
+public class PhysicianAdapter extends RecyclerView.Adapter<PhysicianAdapter.PhysicianViewHolder> {
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view=
-                LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_physicians,viewGroup,false);
-        return new ViewHolder(view);
-    }
+    private final LayoutInflater mInflater;
+    private List<Physician> mPhysicians; // Cached copy of words
+
+    PhysicianAdapter(Context context) { mInflater = LayoutInflater.from(context); }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        Physician physician =physiciansList.get(i);
-
-        //  viewHolder.txtid.setText(md.getId());
-        viewHolder.physicianIdText.setText(physician.getPhysicianId());
-        viewHolder.physicianNameText.setText(physician.getFirstName() + " " + physician.getLastName());
-        viewHolder.physicianEmailText.setText(physician.getEmail());
-        viewHolder.physicianSpecialityText.setText(physician.getSpeciality());
+    public PhysicianViewHolder onCreateViewHolder(ViewGroup parent,
+                                                  int viewType) {
+        View itemView = mInflater.inflate(R.layout.list_physicians, parent, false);
+        return new PhysicianViewHolder(itemView);
     }
 
+    @Override
+    public void onBindViewHolder(PhysicianViewHolder holder, int position) {
+        if (mPhysicians != null) {
+            Physician current = mPhysicians.get(position);
+            holder.physicianIdTextView.setText(current.getPhysicianId());
+            holder.physicianNameTextView.setText(current.getFirstName() + " " + current.getLastName());
+            holder.physicianEmailTextView.setText(current.getEmail());
+            holder.physicianSpecialityTextView.setText(current.getSpeciality());
+
+        } else {
+            // Covers the case of data not being ready yet.
+            holder.physicianIdTextView.setText("N/A");
+            holder.physicianNameTextView.setText("N/A");
+            holder.physicianEmailTextView.setText("N/A");
+            holder.physicianSpecialityTextView.setText("N/A");
+        }
+    }
+
+    void setPhysicians(List<Physician> physicians){
+        mPhysicians = physicians;
+        notifyDataSetChanged();
+    }
+
+    // getItemCount() is called many times, and when it is first called,
+    // mWords has not been updated (means initially, it's null, and we can't return null).
     @Override
     public int getItemCount() {
-        return physiciansList.size();
+        if (mPhysicians != null)
+            return mPhysicians.size();
+        else return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        private TextView physicianIdText,physicianNameText,
-                physicianSpecialityText,
-                physicianEmailText;
-        public ViewHolder(@NonNull View itemView) {
+    class PhysicianViewHolder extends RecyclerView.ViewHolder {
+        private final TextView physicianIdTextView;
+        private final TextView physicianNameTextView;
+        private final TextView physicianEmailTextView;
+        private final TextView physicianSpecialityTextView;
+
+        private PhysicianViewHolder(View itemView) {
             super(itemView);
-            physicianIdText=
-                    (TextView)itemView.findViewById(R.id.physicianIdText);
-            physicianNameText=
-                    (TextView)itemView.findViewById(R.id.physicianNameText);
-            physicianEmailText= (TextView)itemView.findViewById(R.id.physicianEmailText);
-            physicianSpecialityText=(TextView)itemView.findViewById(R.id.physicianSpecialtyText);
+            physicianIdTextView = itemView.findViewById(R.id.physicianIdText);
+            physicianNameTextView = itemView.findViewById(R.id.physicianNameText);
+            physicianEmailTextView = itemView.findViewById(R.id.physicianEmailText);
+            physicianSpecialityTextView = itemView.findViewById(R.id.physicianSpecialtyText);
         }
     }
 }
-
