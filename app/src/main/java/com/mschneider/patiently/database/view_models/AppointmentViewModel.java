460ff9d0e9ae2d3638fc3474c5fc957240301de3
matischneider082@gmail.com <matischneider082@gmail.com>
2021-04-15 12:29:40 -0400

    AppointmentViewModel

--- app/src/main/java/com/mschneider/patiently/database/view_models/AppointmentViewModel.java
+++ app/src/main/java/com/mschneider/patiently/database/view_models/AppointmentViewModel.java
@@ -1,4 +1,30 @@
 package com.mschneider.patiently.database.view_models;
 
-public class AppointmentViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.mschneider.patiently.database.repo.AppointmentRepo;
+import com.mschneider.patiently.database.repo.PatientRepo;
+import com.mschneider.patiently.models.Appointment;
+import com.mschneider.patiently.models.Patient;
+
+import java.util.List;
+
+public class AppointmentViewModel extends AndroidViewModel {
+
+    private AppointmentRepo mRepository;
+
+    private LiveData<List<Appointment>> mAllAppointments;
+
+    public AppointmentViewModel (Application application) {
+        super(application);
+        mRepository = new AppointmentRepo(application);
+        mAllAppointments = mRepository.getAllAppointments();
+    }
+
+    LiveData<List<Appointment>> getAllAppointments() { return mAllAppointments; }
+
+    public void insert(Appointment appointment) { mRepository.insertAppointment(appointment); }
 }
