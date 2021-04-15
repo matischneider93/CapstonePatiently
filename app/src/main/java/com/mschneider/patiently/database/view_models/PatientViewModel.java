f03cfc245d1cdc9745ae04135fa2b23605e60139
matischneider082@gmail.com <matischneider082@gmail.com>
2021-04-15 12:27:20 -0400

    PatientViewModel

--- app/src/main/java/com/mschneider/patiently/database/view_models/PatientViewModel.java
+++ app/src/main/java/com/mschneider/patiently/database/view_models/PatientViewModel.java
@@ -1,4 +1,30 @@
 package com.mschneider.patiently.database.view_models;
 
-public class PatientViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.mschneider.patiently.database.repo.PatientRepo;
+import com.mschneider.patiently.database.repo.PhysicianRepo;
+import com.mschneider.patiently.models.Patient;
+import com.mschneider.patiently.models.Physician;
+
+import java.util.List;
+
+public class PatientViewModel extends AndroidViewModel {
+
+    private PatientRepo mRepository;
+
+    private LiveData<List<Patient>> mAllPatients;
+
+    public PatientViewModel (Application application) {
+        super(application);
+        mRepository = new PatientRepo(application);
+        mAllPatients = mRepository.getAllPatients();
+    }
+
+    LiveData<List<Patient>> getAllPatients() { return mAllPatients; }
+
+    public void insert(Patient patient) { mRepository.insertPatient(patient); }
 }
