8ebd6c48907c74d153f883fec441d25dbe303f66
matischneider082@gmail.com <matischneider082@gmail.com>
2021-05-02 16:03:02 -0400

    Added add screen

--- app/src/main/java/com/mschneider/patiently/ui/activities/physician/PhysicianAddActivity.java
+++ app/src/main/java/com/mschneider/patiently/ui/activities/physician/PhysicianAddActivity.java
@@ -1,12 +1,16 @@
 package com.mschneider.patiently.ui.activities.physician;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mschneider.patiently.R;
+import com.mschneider.patiently.models.Physician;
+import com.mschneider.patiently.ui.activities.MainActivity;
 
 public class PhysicianAddActivity extends AppCompatActivity {
     private EditText physicianFirstNameEditText;
@@ -24,9 +28,24 @@ public class PhysicianAddActivity extends AppCompatActivity {
         physicianFirstNameEditText = findViewById(R.id.physicianFirstNameEditText);
         physicianLastNameEditText = findViewById(R.id.physicianLastNameEditText);
         physicianPhoneEditText = findViewById(R.id.physicianPhoneEditText);
-        physicianEmailEditText = findViewById(R.id.physicianPhoneEditText);
-        physicianSpecialtyEditText = findViewById(R.id.physicianPhoneEditText);
+        physicianEmailEditText = findViewById(R.id.physicianEmailEditText);
+        physicianSpecialtyEditText = findViewById(R.id.physicianSpecialtyEditText);
         physicianUpdateButton = findViewById(R.id.physicianUpdateButton);
+
+
+        physicianUpdateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Physician newPhysician = new Physician(physicianFirstNameEditText.getText().toString(),
+                    physicianLastNameEditText.getText().toString(),
+                    physicianPhoneEditText.getText().toString(),
+                    physicianEmailEditText.getText().toString(),
+                    physicianSpecialtyEditText.getText().toString());
+                MainActivity.getAppDatabase().physicianDao().insertPhysician(newPhysician);
+                Intent intent = new Intent(getApplicationContext(), PhysiciansActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
